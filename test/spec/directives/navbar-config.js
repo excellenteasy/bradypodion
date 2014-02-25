6eba75139898dd47c7a442db13acb1492f555afb
Stephan Bönnemann <stephan@boennemann.me>
2014-02-25 10:56:10 +0100

    test(navbar): animate on ios enter/leave

--- test/spec/directives/navbar-config.js
+++ test/spec/directives/navbar-config.js
@@ -3,14 +3,14 @@ describe('navbarConfig', function() {
 
   it('should be removed from DOM', inject(function($rootScope, $compile) {
 
-    element = angular.element('<div bp-navigation></div>')
-      .append(angular.element(('<bp-navbar-config>')))
+    var $config = angular.element('<bp-navbar-config>')
+    var $element = angular.element('<div bp-navigation></div>')
+      .append($config)
 
-    angular.element('body').append(element)
-    expect($.contains(document, element.children().get(0))).toBe(true)
+    angular.element('body').append($element)
+    expect($.contains(document, $config.get(0))).toBe(true)
 
-    $compile(element)($rootScope)
-    expect($.contains(document, element.children().get(0))).toBe(false)
-
+    $compile($element)($rootScope)
+    expect($.contains(document, $config.get(0))).toBe(false)
   }))
 })
--- test/spec/directives/navigation.js
+++ test/spec/directives/navigation.js
@@ -10,11 +10,21 @@ describe('navigation',function() {
   var $actions = angular.element('<bp-action>')
     .text('Action')
 
-  var state = {
-    name: 'foostate'
+  var fooState = {
+    name: 'foostate',
+    url: '/foourl'
   }
 
-  beforeEach(module('bp'))
+  var barState = {
+    name: 'barstate',
+    url: '/barurl'
+  }
+
+  beforeEach(module('bp', function(bpConfigProvider) {
+    bpConfigProvider.setConfig({
+      platform: 'ios'
+    })
+  }))
 
   beforeEach(inject(function($rootScope, $compile) {
     scope = $rootScope.$new()
@@ -25,7 +35,7 @@ describe('navigation',function() {
   describe('controller', function() {
     it('should register and creat navbar', function() {
 
-      ctrl.registerNavbar(attrs,$actions,state)
+      ctrl.registerNavbar(attrs,$actions,fooState)
 
       var navbarConfig = scope.bpNavbarConfig.foostate
       expect(angular.isObject(navbarConfig)).toBe(true)
@@ -39,19 +49,24 @@ describe('navigation',function() {
 
   describe('link', function() {
     it('should create navbar', function() {
-      ctrl.registerNavbar(attrs,$actions,state)
-      scope.$broadcast('$stateChangeSuccess',state)
-      var $navbar = element.children()
+      ctrl.registerNavbar(attrs,$actions,fooState)
+      scope.$broadcast('$stateChangeSuccess', fooState, null, barState)
+      var $navbar = element.children('bp-navbar-wrapper').children()
       expect($navbar.attr('foo-bar')).toBe('foo')
       expect($navbar.children('bp-action').length).toBe(1)
     })
-    it('should remove old navbar', function() {
+    it('should remove old navbar', inject(function($timeout, bpConfig) {
       angular.element('body').append(element)
-      scope.$broadcast('$stateChangeSuccess',state)
-      var $navbar = element.children()
+      scope.$broadcast('$stateChangeSuccess', fooState, null, barState)
+      var $navbar = element.children('bp-navbar-wrapper').children()
       expect($.contains(document, $navbar.get(0))).toBe(true)
-      scope.$broadcast('$stateChangeSuccess',{name: 'barstate'})
+      scope.$broadcast('$stateChangeSuccess', barState, null, fooState)
+      $timeout.flush()
       expect($.contains(document, $navbar.get(0))).toBe(false)
-    })
+      $navbar = element.children('bp-navbar-wrapper').children()
+      bpConfig.setConfig({platform: 'android'})
+      scope.$broadcast('$stateChangeSuccess', fooState, null, barState)
+      expect($.contains(document, $navbar.get(0))).toBe(false)
+    }))
   })
 })
