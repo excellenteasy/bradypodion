1106fd9c3cfc62f12a4e31162e0bd99dc8faf3ac
Stephan Bönnemann <stephan@boennemann.me>
2014-02-27 19:22:54 +0100

    feat(sref): allow ui-router style param definition

--- modules/scripts/directives/sref.js
+++ modules/scripts/directives/sref.js
@@ -1,12 +1,25 @@
 angular.module('bp').directive('bpSref', function($state, $parse, BpTap) {
-  return function(scope, element, attrs) {
-    new BpTap(scope, element, attrs)
-    element.bind('tap', function() {
-      $state.transitionTo(attrs.bpSref)
-      return false
-    })
-    scope.$on('$destroy', function() {
-      element.unbind('tap')
-    })
+  return {
+    controller: function($scope) {
+      // https://github.com/angular-ui/ui-router/blob/master/src/stateDirectives.js#L1-L5
+      this.parseStateRef = function(ref) {
+        var parsed = ref.replace(/\n/g, ' ').match(/^([^(]+?)\s*(\((.*)\))?$/)
+        return {
+          state: parsed[1],
+          params: parsed[3] ? $parse(parsed[3])($scope) : null
+        }
+      }
+    },
+    link: function(scope, element, attrs, ctrl) {
+      var ref = ctrl.parseStateRef(attrs.bpSref)
+      new BpTap(scope, element, attrs)
+      element.bind('tap', function() {
+        $state.go(ref.state,ref.params)
+        return false
+      })
+      scope.$on('$destroy', function() {
+        element.unbind('tap')
+      })
+    }
   }
 })
