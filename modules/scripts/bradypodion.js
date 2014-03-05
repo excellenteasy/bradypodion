0f40a6bd23c7cb2f91ab0d8675edf3decd4120c9
Stephan Bönnemann <stephan@boennemann.me>
2014-03-05 21:06:18 +0100

    docs(bp): overview for the bp module

--- modules/scripts/bradypodion.js
+++ modules/scripts/bradypodion.js
@@ -1,1 +1,12 @@
+/**
+@ngdoc overview
+@name bp
+@description
+# The `bp` module
+All Bradypodion services and directives are exposed on the `bp` module.
+It requires `ngAnimate` and `ui.router`. Use it with your app likes this:
+```js
+angular.module('yourApp', ['bp'])
+```
+*/
 angular.module('bp', ['ngAnimate', 'ui.router'])
