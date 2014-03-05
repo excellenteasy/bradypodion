9d8009bdd8e999406b218fbc15d9aade35959254
Stephan Bönnemann <stephan@boennemann.me>
2014-03-05 21:08:11 +0100

    docs(services): basics for config, sref, tap and view

--- modules/scripts/services/config.js
+++ modules/scripts/services/config.js
@@ -1,3 +1,7 @@
+/**
+@ngdoc object
+@name bp.bpConfigProvider
+*/
 angular.module('bp').provider('bpConfig', (function() {
   var config = {
     platform: 'ios'
@@ -7,14 +11,39 @@ angular.module('bp').provider('bpConfig', (function() {
     config.setConfig = this.setConfig
   }
 
+  /**
+  @ngdoc object
+  @name bp.bpConfig
+  @description
+  Contains the Bradypodion configuration object, which is used internally for post-platform abstractions.
+  */
   BpConfig.prototype.$get = function() {
     return config
   }
 
+  /**
+  @ngdoc function
+  @name bp.bpConfigProvider#setConfig
+  @methodOf bp.bpConfigProvider
+  @param {object} config custom configuration
+  @description Allows to modify Bradypodion configuration.
+  The default configuration is `{platform: 'ios'}`.
+  You may change the platform globally, or set some iScroll settings on `config.iscoll`.
+  */
+  /**
+  @ngdoc function
+  @name bp.bpConfig#setConfig
+  @methodOf bp.bpConfig
+  @param {object} config custom configuration
+  @description
+  <div class="alert">
+    Using this method on runtime may cause an inconsistent app state.
+    It's recommend to use it on the {@link bp.bpConfigProvider#setConfig provider}.
+  </div>
+  */
   BpConfig.prototype.setConfig = function(inConfig) {
     config = angular.extend(config, inConfig)
   }
 
   return BpConfig
-
 })())
--- modules/scripts/services/sref.js
+++ modules/scripts/services/sref.js
@@ -1,4 +1,23 @@
+/**
+@ngdoc service
+@name bp.bpSref
+@description Services that parses state references.
+Used internally by {@link bp.directive:bpSref} and {@link bp.directive:bpNavbar}.
+*/
 angular.module('bp').service('bpSref', function($parse) {
+  /**
+  @ngdoc function
+  @name bp.bpSref#parse
+  @description
+  ```javascript
+  bpSref.parse('customer({id: id})', scope)
+  // {state: 'customer', params: {id: 5}}
+  ```
+  @methodOf bp.bpSref
+  @param {string} ref The state reference with optional parameters.
+  @param {scope} scope The scope the params should be parsed against.
+  @returns {object} Contains parsed `state` and `params`.
+  */
   this.parse = function(ref, scope) {
     var parsed = ref.replace(/\n/g, ' ').match(/^([^(]+?)\s*(\((.*)\))?$/)
     return {
