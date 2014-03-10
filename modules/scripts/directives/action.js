167e435d0b44b12f7c7dec92c3ce6dbad5615204
Stephan Bönnemann <stephan@boennemann.me>
2014-03-10 13:27:11 +0100

    docs(action): intial docs

--- modules/scripts/directives/action.js
+++ modules/scripts/directives/action.js
@@ -1,3 +1,15 @@
+/**
+@ngdoc directive
+@name bp.directive:bpAction
+@restrict E
+@param {string} class Add either the `bp-button` or `bp-icon` class to an action element.
+@description
+`bpAction` is a general type of interface element that is mainly used within `bpNavbar`.
+It supports `bpButton` and `bpIcon` types.
+The abstraction is necessary as in some occasions an action is an icon on android, while it's a button on ios.
+This is described in more detail in the {@link bp.directive:bpNavbar `bpNavbar`} documentation.
+ */
+
 angular.module('bp').directive('bpAction', function() {
   return {
     restrict: 'E',
