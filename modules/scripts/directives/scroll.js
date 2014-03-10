3651777509f8585119407d406cfafae39580dcf4
Stephan Bönnemann <stephan@boennemann.me>
2014-03-10 17:05:46 +0100

    docs(scroll): initial docs

--- modules/scripts/directives/scroll.js
+++ modules/scripts/directives/scroll.js
@@ -1,3 +1,23 @@
+/**
+@ngdoc directive
+@name bp.directive:bpScroll
+@param {boolean} bpScrollSticky Enables sticky headers for `ios`.
+@example
+<pre>
+<bp-table bp-scroll bp-scroll-sticky>
+  <div>
+    <bp-table-header>A</bp-table-header>
+    <bp-cell>Aa</bp-cell>
+    <bp-cell>Aa</bp-cell>
+    <bp-cell>Aa</bp-cell>
+    <bp-cell>Aa</bp-cell>
+    <bp-cell>Aa</bp-cell>
+  </div>
+</bp-table>
+</pre>
+@description Makes the content within the directive scrollable via CSS.
+*/
+
 angular.module('bp').directive('bpScroll', function() {
   return function(scope, element) {
     element.bind('touchstart', angular.noop)
