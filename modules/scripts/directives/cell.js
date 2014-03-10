b4c74618173f1c42c819e995cf54e31ae8a7527b
Stephan Bönnemann <stephan@boennemann.me>
2014-03-10 15:41:03 +0100

    docs(cell): initial docs

--- modules/scripts/directives/cell.js
+++ modules/scripts/directives/cell.js
@@ -1,3 +1,38 @@
+/**
+@ngdoc directive
+@name bp.directive:bpCell
+@restrict E
+@param {string} class You can define different cell styles.
+
+`bp-cell-value1` | `bp-cell-value2` | `bp-cell-subtitle`
+@example
+<pre>
+  <bp-cell>Cell</bp-cell>
+</pre>
+<pre>
+  <bp-cell class="bp-cell-value1">
+    <div class="label">Label</div>
+    <div class="value">Value</div>
+  </bp-cell>
+</pre>
+<pre>
+  <bp-cell class="bp-cell-value2">
+    <div class="label">Label</div>
+    <div class="value">Value</div>
+  </bp-cell>
+</pre>
+<pre>
+  <bp-cell class="bp-cell-subtitle">
+    <div class="titles">
+      <div class="title">Title</div>
+      <div class="subtitle">Subtitle</div>
+    </div>
+  </bp-cell>
+</pre>
+@description
+`bpCell` is a interface element that, together with other cells, constitutes a {@link bp.bpTable `bpTable`}.
+ */
+
 angular.module('bp').directive('bpCell', function() {
   return {
     restrict: 'E',
