fa579fada8408b8212e44d581a83f1ef92795fa2
Stephan Bönnemann <stephan@boennemann.me>
2014-03-10 23:26:44 +0100

    docs(tab): initial docs

--- modules/scripts/directives/tabbar.js
+++ modules/scripts/directives/tabbar.js
@@ -16,6 +16,27 @@ angular.module('bp').directive('bpTabbar', function() {
   }
 })
 
+/**
+@ngdoc directive
+@restrict E
+@name bp.directive:bpTab
+@scope true
+@param {string} bpSref The name of the state the tab is associated to.
+@param {string} bpTabIcon A CSS class that represents a font-icon for the tab.
+@param {string=} bpTabTitle The title of the tab. By default it's read from the associated state.
+@description A tab withing a  {@link bp.directive:bpTabbar `bpTabbar`}.
+@example
+<pre>
+<bp-tabbar>
+  <bp-tab bp-sref='first'  bp-tab-icon="icon-bookmark"></bp-tab>
+  <bp-tab bp-sref='second' bp-tab-icon="icon-comment"></bp-tab>
+  <bp-tab bp-sref='third'  bp-tab-icon="icon-download"></bp-tab>
+  <bp-tab bp-sref='fourth' bp-tab-icon="icon-inbox"></bp-tab>
+  <bp-tab bp-sref='fifth'  bp-tab-icon="icon-music"></bp-tab>
+</bp-tabbar>
+</pre>
+*/
+
 angular.module('bp').directive('bpTab', function($state, $compile, $timeout) {
   return {
     restrict: 'E',
