4160dfec901e6a212421eadd30971258c34469e9
Stephan Bönnemann <stephan@boennemann.me>
2014-03-10 13:26:58 +0100

    docs(sref): intial docs

--- modules/scripts/directives/sref.js
+++ modules/scripts/directives/sref.js
@@ -1,3 +1,20 @@
+/**
+@ngdoc directive
+@name bp.directive:bpSref
+@requires bp.util.bpTap
+@restrict A
+@element ANY
+@param {string} bpSref Name of the state to transition to. You can define params for the state like this:
+`statename({id: 1})`,
+@description
+`bpSref` is similiar to Angular UI Router's own `uiSref` directive,
+as it transitions to a state, but without 300ms delay.
+
+<div class="alert alert-info">
+  The tap behavior may be changed with the same attriubutes as with {@link bp.directive:bpTap `bpTap`}.
+</div>
+ */
+
 angular.module('bp').directive('bpSref', function($state, $parse, bpTap, bpView) {
   return function(scope, element, attrs) {
     var ref = bpView.parseState(attrs.bpSref, scope)
