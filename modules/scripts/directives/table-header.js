5b39078b2a0fbae126acc34c68de3b0458bfad97
Stephan Bönnemann <stephan@boennemann.me>
2014-03-11 09:49:37 +0100

    docs(tableHeader): initial docs

--- modules/scripts/directives/table-header.js
+++ modules/scripts/directives/table-header.js
@@ -1,3 +1,11 @@
+/**
+@ngdoc directive
+@name bp.directive:bpTableHeader
+@restrict E
+@description `bpTableHeader` is used as a header element withing {@link bp.directive:bpTable `bpTable`}s.
+If you enable either `bpScrollSticky` or `bpIscrollSticky` it's the default element to stick to the top.
+ */
+
 angular.module('bp').directive('bpTableHeader', function() {
   return {
     restrict: 'E',
