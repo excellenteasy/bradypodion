8f46abdd3f5404cf3432896de801533468531ff3
Stephan Bönnemann <stephan@boennemann.me>
2014-03-12 11:44:47 +0100

    docs(navbar-config): initial docs

--- modules/scripts/directives/navbar-config.js
+++ modules/scripts/directives/navbar-config.js
@@ -1,3 +1,21 @@
+/**
+@ngdoc directive
+@name bp.directive:bpNavbarConfig
+@restrict E
+@requires bp.directive:bpNavigation
+@description `bpNavbarConfig` is a way to configure navbars injected by the {@link bp.bpNavigation `bpNavigation`} directive.
+It offers exactly the {@link bp.directive:bpNavbar#usage_parameters same attributes} for configuration as the original navbar, and forwards them to it.
+If you put `bpAction`s into the directive they will be moved to the original navbar.
+@example
+<pre>
+<bp-navbar-config bp-navbar-no-up>
+  <bp-action class="icon-checkmark">
+    Done
+  </bp-action>
+</bp-navbar-config>
+</pre>
+*/
+
 angular.module('bp').directive('bpNavbarConfig', function($state) {
   return {
     restrict: 'E',
